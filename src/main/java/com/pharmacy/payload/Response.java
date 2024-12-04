ac87ec32b69010d207e9845bd24d4e5cb19f8a5d
octavian.ivan <i.octavian900@gmail.com>
2024-12-05 00:14:28 +0200

    Created a generic response class.

---
+++ src/main/java/com/pharmacy/payload/Response.java
@@ -0,0 +1,39 @@
+package com.pharmacy.payload;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonProperty("timeStamp")
+    protected LocalDateTime timeStamp = LocalDateTime.now();
+
+    @JsonProperty("status")
+    protected Status status;
+
+    @JsonProperty("message")
+    protected String message;
+
+    @JsonProperty("data")
+    protected T data;
+
+    public Response(Status status, String message) {
+        this.timeStamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message;
+    }
+    public Response(Status status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+    public enum Status {
+        SUCCESS, ERROR, FAIL
+    }
+}
