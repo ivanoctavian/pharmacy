ea4b8d53e2b6fdc9e8ba5a54db395c5f5cfd8110
Ivan Octavian <ivan@transferrapid.com>
2024-12-05 11:36:45 +0200

    Refactored names from plural to singular.

--- src/main/java/com/pharmacy/modules/inventory/model/Supplier.java
+++ src/main/java/com/pharmacy/modules/inventory/model/Supplier.java
@@ -7,18 +7,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Suppliers")
+@Table(name = "Supplier")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Suppliers {
+public class Supplier {
     @Id
     @Column(name = "id", unique = true, nullable = false)
-    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "Suppliers_seq")
-    @SequenceGenerator(name="Suppliers_seq", sequenceName="Suppliers_seq", allocationSize=1)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "Supplier_seq")
+    @SequenceGenerator(name="Supplier_seq", sequenceName="Supplier_seq", allocationSize=1)
     private Long id;
-
 
     @Column(name = "name")
     private String name;
--- src/main/java/com/pharmacy/modules/inventory/repository/SupplierRepository.java
+++ src/main/java/com/pharmacy/modules/inventory/repository/SupplierRepository.java
@@ -1,9 +1,9 @@
 package com.pharmacy.modules.inventory.repository;
 
-import com.pharmacy.modules.inventory.model.Suppliers;
+import com.pharmacy.modules.inventory.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SuppliersRepository extends JpaRepository<Suppliers, Integer>{
+public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
 }
