2bd5aec84c6874cab15a10042f93405773334631
Ivan Octavian <ivan@transferrapid.com>
2024-12-05 16:42:13 +0200

    Added @Mandatory annotation for supplierName.

--- src/main/java/com/pharmacy/payload/SupplierRequest.java
+++ src/main/java/com/pharmacy/payload/SupplierRequest.java
@@ -1,10 +1,11 @@
 package com.pharmacy.payload;
 
 
+import com.pharmacy.config.Mandatory;
 import lombok.Data;
 
 @Data
 public class SupplierRequest {
-
+    @Mandatory
     private String supplierName;
 }
