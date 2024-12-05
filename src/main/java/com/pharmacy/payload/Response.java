0b096a48ac64d7d98596272387bc437c8ddbce63
Ivan Octavian <ivan@transferrapid.com>
2024-12-05 12:09:25 +0200

    Added @NoArgsConstructor.

--- src/main/java/com/pharmacy/payload/Response.java
+++ src/main/java/com/pharmacy/payload/Response.java
@@ -3,13 +3,14 @@ package com.pharmacy.payload;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
 public class Response<T> {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonProperty("timeStamp")
