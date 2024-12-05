952c29c9c2d41e36c40a1c11610cc5c112b07d65
Ivan Octavian <ivan@transferrapid.com>
2024-12-05 14:42:50 +0200

    Added @ToString method to create more readable logs.

--- src/main/java/com/pharmacy/modules/inventory/model/Supplier.java
+++ src/main/java/com/pharmacy/modules/inventory/model/Supplier.java
@@ -1,10 +1,7 @@
 package com.pharmacy.modules.inventory.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "Supplier")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Supplier {
     @Id
     @Column(name = "id", unique = true, nullable = false)
