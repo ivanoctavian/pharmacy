cef4c2decc256d330c1792cc7aa98f60ac0d1249
Ivan Octavian <ivan@transferrapid.com>
2024-12-05 12:09:46 +0200

    Added findByName method parametrized.

--- src/main/java/com/pharmacy/modules/inventory/repository/SupplierRepository.java
+++ src/main/java/com/pharmacy/modules/inventory/repository/SupplierRepository.java
@@ -4,6 +4,10 @@ import com.pharmacy.modules.inventory.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
+    Optional<Supplier> findByName(String name);
 }
