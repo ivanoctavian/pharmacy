7f9160de707c98e5e6eda12d46e1f04608229361
Ivan Octavian <ivan@transferrapid.com>
2024-12-11 18:51:04 +0200

    Creted class for making request to a 3rd party api to check if a medicine exists.

---
+++ src/main/java/com/pharmacy/modules/approved_medicines/service/ApiFdaService.java
@@ -0,0 +1,12 @@
+package com.pharmacy.modules.approved_medicines.service;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Slf4j
+public class ApiFdaService {
+
+}
