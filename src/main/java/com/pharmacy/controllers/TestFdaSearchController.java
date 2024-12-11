6b30bafada229f7fd7388bb7a4987da24c87094f
Ivan Octavian <ivan@transferrapid.com>
2024-12-11 22:37:35 +0200

    Changed the return type for the /fda-api/search endpoint

--- src/main/java/com/pharmacy/controllers/TestFdaSearchController.java
+++ src/main/java/com/pharmacy/controllers/TestFdaSearchController.java
@@ -1,7 +1,10 @@
 package com.pharmacy.controllers;
 
+import com.pharmacy.modules.approved_medicines.model.FdaApprovedMedicine;
 import com.pharmacy.modules.approved_medicines.service.ApiFdaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,7 @@ public class TestFdaSearchController {
     private final ApiFdaService apiFdaService;
 
     @GetMapping("/search")
-    public String searchFdaByName(String medicineName){
-        return apiFdaService.searchForMedicineByName(medicineName);
+    public ResponseEntity<FdaApprovedMedicine> searchFdaByName(String medicineName){
+        return new ResponseEntity<>(apiFdaService.searchForMedicineByName(medicineName), HttpStatus.OK);
     }
 }
