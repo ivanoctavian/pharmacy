e860933bc23449e40af8e53344931820bda242fb
Ivan Octavian <ivan@transferrapid.com>
2024-12-12 10:17:19 +0200

    Created the entities for tables: Medicine and Category

---
+++ src/main/java/com/pharmacy/modules/inventory/model/Category.java
@@ -0,0 +1,27 @@
+package com.pharmacy.modules.inventory.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Category {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "Category_seq")
+    @SequenceGenerator(name="Category_seq", sequenceName="Category_seq", allocationSize=1)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "require_recipe")
+    private boolean requiresRecipe = false;
+}
---
+++ src/main/java/com/pharmacy/modules/inventory/model/Medicine.java
@@ -0,0 +1,49 @@
+package com.pharmacy.modules.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Medicine")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Medicine {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "Medicine_seq")
+    @SequenceGenerator(name="Medicine_seq", sequenceName="Medicine_seq", allocationSize=1)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
+
+    @Column(name = "stock")
+    private Long stock;
+
+    @Column(name = "substance_name")
+    private String substanceName;
+
+    @Column(name = "producer")
+    private String producer;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id", foreignKey = @ForeignKey(name = "FK_SUPPLIER_ID"))
+    private Supplier supplier;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", foreignKey = @ForeignKey(name = "FK_CATEGORY_ID"))
+    private Category category;
+}
