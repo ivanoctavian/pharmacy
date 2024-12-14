b9f05d574440a64647fe48b3086f097881072b0c
Ivan Octavian <ivan@transferrapid.com>
2024-12-14 19:06:43 +0200

    fix: deleted unnecessary import

--- src/main/java/com/pharmacy/modules/approved_medicines/service/ApiFdaService.java
+++ src/main/java/com/pharmacy/modules/approved_medicines/service/ApiFdaService.java
@@ -2,6 +2,7 @@ package com.pharmacy.modules.approved_medicines.service;
 
 import com.pharmacy.config.ApiException;
 import com.pharmacy.modules.approved_medicines.model.FdaApprovedMedicine;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
