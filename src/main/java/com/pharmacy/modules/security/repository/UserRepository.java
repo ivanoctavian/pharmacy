70d5fbed0ea465d1f14559ac4cdfb852ad91f62c
Ivan Octavian <ivan@transferrapid.com>
2024-12-16 15:54:10 +0200

    Added method for finding user by username and pwd.

--- src/main/java/com/pharmacy/modules/security/repository/UserRepository.java
+++ src/main/java/com/pharmacy/modules/security/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.pharmacy.modules.security.repository;
 
-public interface UserRepository {
+import com.pharmacy.modules.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
