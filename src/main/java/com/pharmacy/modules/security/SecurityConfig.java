770137579b6824919e14175f3cb6f20490e09b98
octavian.ivan <i.octavian900@gmail.com>
2024-12-16 21:57:58 +0200

    Implemented security on endpoint. H2 console - unsecured.

--- src/main/java/com/pharmacy/modules/security/SecurityConfig.java
+++ src/main/java/com/pharmacy/modules/security/SecurityConfig.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @AllArgsConstructor
@@ -16,22 +17,33 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     private final CustomAuthenticationManager customAuthenticationManager;
     private final CustomBasicAuthProvider customBasicAuthProvider;
+    private final CustomAccessDeniedHandler handler;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
     @Bean
     SecurityFilterChain securityFilterChain1(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/medicines/**").hasRole("EMPLOYEE");
+                    auth.requestMatchers("/medicines/**").hasAnyRole("ADMIN", "EMPLOYEE");
                     auth.requestMatchers("/suppliers/**").permitAll();
                     auth.requestMatchers("/h2-console/**").permitAll();
-                    auth.anyRequest().permitAll();
+                    auth.anyRequest().authenticated();
                 })
                 .authenticationProvider(customBasicAuthProvider)
                 .authenticationManager(customAuthenticationManager)
                 .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(exc -> {
+                    exc.accessDeniedHandler(handler);
+                    exc.authenticationEntryPoint(authenticationEntryPoint);
+                })
                 .cors(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        ;
+                //below is used for having access to h2 console.
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                        .disable())
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
     }
 }
